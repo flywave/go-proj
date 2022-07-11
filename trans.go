16837d5b5710e7bfedcc3ddf2b2646f99fd98a90
hj <hawking2215@gmail.com>
2022-07-11 11:14:53 +0800

    update

--- trans.go
+++ trans.go
@@ -12,28 +12,34 @@ const (
 	Rad2Deg = 180 / math.Pi
 )
 
+var (
+	wgs84Proj *Proj
+	ecefProj  *Proj
+	mercProj  *Proj
+)
+
+func init() {
+	wgs84Proj, _ = NewProj("+proj=longlat +datum=WGS84 +no_defs")
+	ecefProj, _ = NewProj("+proj=geocent +datum=WGS84 +units=m +no_defs")
+	mercProj, _ = NewProj("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over")
+}
+
 func Lonlat2Ecef(lon, lat, z float64) (float64, float64, float64, error) {
-	wgs84Proj, _ := NewProj("+proj=longlat +datum=WGS84 +no_defs")
-	ecefProj, _ := NewProj("+proj=geocent +datum=WGS84 +units=m +no_defs")
 	return Transform3(wgs84Proj, ecefProj, lon*Deg2Rad, lat*Deg2Rad, z)
 }
 
 func Ecef2Lonlat(x, y, z float64) (float64, float64, float64, error) {
-	wgs84Proj, _ := NewProj("+proj=longlat +datum=WGS84 +no_defs")
-	ecefProj, _ := NewProj("+proj=geocent +datum=WGS84 +units=m +no_defs")
+
 	lon, lat, z, err := Transform3(ecefProj, wgs84Proj, x, y, z)
 	return lon * Rad2Deg, lat * Rad2Deg, z, err
 }
 
 func Lonlat2Merc(lon, lat float64) (float64, float64, error) {
 	wgs84Proj, _ := NewProj("+proj=longlat +datum=WGS84 +units=m +no_defs")
-	mercProj, _ := NewProj("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over")
 	return Transform2(wgs84Proj, mercProj, lon*Deg2Rad, lat*Deg2Rad)
 }
 
 func Merc2Lonlat(x, y float64) (float64, float64, error) {
-	wgs84Proj, _ := NewProj("+proj=longlat +datum=WGS84 +units=m +no_defs")
-	mercProj, _ := NewProj("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over")
 	lon, lat, err := Transform2(mercProj, wgs84Proj, x, y)
 	return lon * Rad2Deg, lat * Rad2Deg, err
 }
